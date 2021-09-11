b6794d4d37dce1884195d5b8d62ae028ec7bba4c
Jan-Rudolph Buhrmann <jrb@s2c.co.za>
2021-09-11 16:18:43 +0200

    Using github secrets for dockerhub credentials

--- hello_svc/src/main/java/s2c/hello_svc/HelloApplication.java
+++ hello_svc/src/main/java/s2c/hello_svc/HelloApplication.java
@@ -18,7 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"s2c.hello_svc"})
 public class HelloApplication {
-//
+
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
