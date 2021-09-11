abe8281013e213837f6b5f0b60a73c89017f182d
Jan-Rudolph Buhrmann <jrb@s2c.co.za>
2021-09-11 16:10:19 +0200

    Using github secrets for dockerhub credentials

--- hello_svc/src/main/java/s2c/hello_svc/HelloController.java
+++ hello_svc/src/main/java/s2c/hello_svc/HelloController.java
@@ -13,7 +13,6 @@
  */
 package s2c.hello_svc;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
